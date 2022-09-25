b6d6e307ba6f89a0ae54724c3e625b83f2465b91
LynxUtz <114174878+LynxUtz@users.noreply.github.com>
2022-09-25 13:03:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-// My first JS file in Github !
+let focusX, focusY, focusZ;
+let alpha, beta;
+let Fdelta, Adelta;
+let distToCenter;
+
+let temp;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  
+  noStroke();
+  
+  focusX = 0;
+  focusY = 0;
+  focusZ = 0;
+  alpha = 0;
+  beta = 45;
+  Fdelta = 5;
+  Adelta = 0.01;
+  distToCenter = 40;
+}
+
+function draw() {
+  background(220);
+}
